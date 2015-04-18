debf393c24c34b65d958a1049a592e66f37d5adb
tete94 <enricobarbieric@gmail.com>
2015-04-17 20:09:03 -0600

    constructor y destructor + métodos de existencia de vertices y adyacencias

--- Grafo.cpp
+++ Grafo.cpp
@@ -8,6 +8,11 @@
 #include "Grafo.h"
 
 Grafo::Grafo(int cntVrt, int prmAdy) {
+    if (cntVrt >= 10 && 1 <= prmGrd)
+    {
+        this->cntVrt = cntVrt;
+        arrVrt = new NdoVrt[cntVrt];
+    }
 }
 
 Grafo::Grafo(const Grafo& orig) {
@@ -17,12 +22,31 @@ Grafo::Grafo(string nArch) {
 }
 
 Grafo::~Grafo() {
+    if (cntVrt > 0)
+        delete[] arrVrt;
 }
 
 bool Grafo::xstVrt(int vrt) const {
+    return (vrt < cntVrt && vrt >= 0); // Devuelve true si vrt esta entre 0 y cntVrt
 }
 
 bool Grafo::xstAdy(int vrtO, int vrtD) const {
+    if (xstVrt(vrt0)) // Comprueba que el indice del vertice existe
+    {
+        int cantidad_adyacencias = arrVrt[vrt0].lstAdy.obtCntAdy();
+        if (cantidad_adyacencias == 0) return false; // No hay adyacencias, por lo tanto la adyacencia no existe
+        int *ady = arrVrt[vrt0].lstAdy.obtCntAdy();
+        for (int i = 0; i < cantidad_adyacencias; i++)
+        {
+            if (ady[i] == vrtD) // Encontró una adyacencia
+            {
+                delete[] ady; // Libera la memoria
+                return true;
+            }
+        }
+        delete[] ady; // Libera la memoria
+    }
+    return false; // No se encontró adyacencia o el vértice no existe
 }
 
 int* Grafo::obtAdy(int vrt) const {
