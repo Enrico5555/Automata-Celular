7946b5885b308001e49d17555101aff3a5a65708
unknown <B33504@ECCI-LBW-105-20.ecci.ucr.ac.cr>
2015-04-27 12:41:10 -0600

    pruebas ++

--- pba_grafo.cpp
+++ pba_grafo.cpp
@@ -20,136 +20,198 @@
  
  //papas
  
-//Construye el grafo correcto con el “grafo_pequenyo.txt”. 
- void testConstructorGrafo1()
- {
-     Grafo grafo(100, 15);
-     if (grafo.obtTotVrt() != 100 || grafo.obtPrmAdy() != 15)
-     {//imi
-         printerror("testConstructorGrafo1");
+//Construye una red con la cantidad de vértices y el promedio de
+//adyacencias por vértice correctos cuando cntVrt == 100 y prmAdy ==15.
+void testConstructorGrafo1(){
+     Grafo grafo(100, 15);// prom +- desviac ... +-3
+     if (grafo.obtTotVrt() != 100 || grafo.obtPrmAdy() != 15){
+      std::cout << "%TEST_FAILED% FALLO EL CONNSTRUCTOR GRAFO 1 (newsimpletest) message=error message sample" << std::endl;
      }
  }
  
- //Construye una red con la cantidad de vértices y el promedio de adyacencias por vértice correctos cuando cntVrt == 1000 y prmAdy == 15.
- void testConstructorGrafo2()
- {
+  //Construye una red con la cantidad de vértices y el promedio de
+//adyacencias por vértice correctos cuando cntVrt == 1000 y prmAdy ==15.
+ void testConstructorGrafo2(){
      Grafo grafo(1000, 15);
      if (grafo.obtTotVrt() != 1000 || grafo.obtPrmAdy() != 15)
      {
-         printerror("testConstructorGrafo2");
+      std::cout << "%TEST_FAILED% FALLO EL CONNSTRUCTOR GRAFO 2 (newsimpletest) message=error message sample" << std::endl;
      }
  }
-
- void testGrafo2() {
+ 
+//Construye una copia idéntica a Grafo(100,15).
+ void testConstGrafo1() {
+    Grafo grafo(100, 15); //usar operator ==
     const Grafo& orig;
-    Grafo grafo(orig);
-    if (true /*check result*/) {
-        std::cout << "%TEST_FAILED% time=0 testname=testGrafo2 (newsimpletest) message=error message sample" << std::endl;
+    Grafo grafo2(orig);
+    if (!(grafo == grafo2) ) {
+        std::cout << "%TEST_FAILED% FALLO EL CONSTRUCTOR DE COPIAS 1 (newsimpletest) message=error message sample" << std::endl;
+    }
+}
+ 
+ //Construye una copia idéntica a Grafo(1000,15).
+ void testConstGrafo2() {
+  Grafo grafo(1000, 15);// op ==
+    const Grafo& orig;
+    Grafo grafo2(orig);
+    if (!(grafo== grafo2) ) {
+        std::cout << "%TEST_FAILED% FALLO EL CONSTRUCTOR DE COPIAS 2 (newsimpletest) message=error message sample" << std::endl;
     }
 }
 
-void testGrafo3() {
+//Construye el grafo correcto con el “grafo_pequenyo.txt”
+void testGrafoString1() {
     string nArch;
-    Grafo grafo(nArch);
-    if (true /*check result*/) {
-        std::cout << "%TEST_FAILED% time=0 testname=testGrafo3 (newsimpletest) message=error message sample" << std::endl;
+    Grafo grafo("redMuyPeq");
+    int a[] = {9}; // comparac cnt y promedio que este en el rango correcto
+    int* b = grafo.obtAdy(3);
+    if (!(grafo.obtTotVrt()==10)|| (grafo.obtTotAdy(8) == 0) ||(b[0]== a[0] ) ) {
+        std::cout << "%TEST_FAILED% FALLO EL METODO STRING 1 (newsimpletest) message=error message sample" << std::endl;
+    }
+    delete []b;
+}
+
+//Construye el grafo correcto con el “grafo_grande.txt”.
+void testGrafoString2() {
+    string nArch;
+    Grafo grafo("redGrn");
+    if (!(grafo.obtTotVrt()==1000)|| (grafo.obtTotAdy(12) == 54) ) {
+        std::cout << "%TEST_FAILED% FALLO EL METODO STRING 1 (newsimpletest) message=error message sample" << std::endl;
     }
 }
 
-void testCentralidadIntermedial() {
-    int vrt;
+//Retorna true cuando el vértice existe.
+void testXstVrt1() {
+    int vrt = 0;
     Grafo grafo;
-    double result = grafo.centralidadIntermedial(vrt);
-    if (true /*check result*/) {
-        std::cout << "%TEST_FAILED% time=0 testname=testCentralidadIntermedial (newsimpletest) message=error message sample" << std::endl;
+    bool result = grafo.xstVrt(vrt);
+    if (result == true) {
+        std::cout << "%TEST_FAILED% FALLO EL METODO XstVrt1 (newsimpletest) message=error message sample" << std::endl;
     }
 }
 
-void testCoeficienteAgrupamiento() {
-    int vrt;
+//Retorna false cuando no existe el vértice
+void testXstVrt2() {
+    int vrt = 0;
     Grafo grafo;
-    double result = grafo.coeficienteAgrupamiento(vrt);
-    if (true /*check result*/) {
-        std::cout << "%TEST_FAILED% time=0 testname=testCoeficienteAgrupamiento (newsimpletest) message=error message sample" << std::endl;
+    bool result = grafo.xstVrt(2);
+    if (result == true) {
+        std::cout << "%TEST_FAILED% FALLO EL METODO XstVrt2 (newsimpletest) message=error message sample" << std::endl;
     }
 }
 
-void testModEst() {
-    int vrt;
-    E ne;
-    Grafo grafo;
-    grafo.modEst(vrt, ne);
-    if (true /*check result*/) {
-        std::cout << "%TEST_FAILED% time=0 testname=testModEst (newsimpletest) message=error message sample" << std::endl;
-    }
-}
-
-void testObtAdy() {
-    int vrt;
-    Grafo grafo;
-    int* result = grafo.obtAdy(vrt);
-    if (true /*check result*/) {
-        std::cout << "%TEST_FAILED% time=0 testname=testObtAdy (newsimpletest) message=error message sample" << std::endl;
-    }
-}
-
-void testObtEst() {
-    int vrt;
-    Grafo grafo;
-    E result = grafo.obtEst(vrt);
-    if (true /*check result*/) {
-        std::cout << "%TEST_FAILED% time=0 testname=testObtEst (newsimpletest) message=error message sample" << std::endl;
-    }
-}
-
-void testObtPrmAdy() {
-    Grafo grafo;
-    double result = grafo.obtPrmAdy();
-    if (true /*check result*/) {
-        std::cout << "%TEST_FAILED% time=0 testname=testObtPrmAdy (newsimpletest) message=error message sample" << std::endl;
-    }
-}
-
-void testObtTotAdy() {
-    Grafo grafo;
-    int result = grafo.obtTotAdy();
-    if (true /*check result*/) {
-        std::cout << "%TEST_FAILED% time=0 testname=testObtTotAdy (newsimpletest) message=error message sample" << std::endl;
-    }
-}
-
-void testObtTotVrt() {
-    Grafo grafo;
-    int result = grafo.obtTotVrt();
-    if (true /*check result*/) {
-        std::cout << "%TEST_FAILED% time=0 testname=testObtTotVrt (newsimpletest) message=error message sample" << std::endl;
-    }
-}
-
-void testPromLongCmnsCrts() {
-    Grafo grafo;
-    double result = grafo.promLongCmnsCrts();
-    if (true /*check result*/) {
-        std::cout << "%TEST_FAILED% time=0 testname=testPromLongCmnsCrts (newsimpletest) message=error message sample" << std::endl;
-    }
-}
-
-void testXstAdy() {
+//Retorna true cuando la adyacencia sí existe.
+void testXstAdy1() {
     int vrtO;
     int vrtD;
     Grafo grafo;
     bool result = grafo.xstAdy(vrtO, vrtD);
-    if (true /*check result*/) {
-        std::cout << "%TEST_FAILED% time=0 testname=testXstAdy (newsimpletest) message=error message sample" << std::endl;
+    if (result == false ) {
+        std::cout << "%TEST_FAILED% FALLO EL METODO XstAdy1 (newsimpletest) message=error message sample" << std::endl;
     }
 }
 
-void testXstVrt() {
+//Retorna false cuando la adyacencia no existe.
+void testXstAdy2() {
+    int vrtO;
+    int vrtD = 0;
+    Grafo grafo; // usar el pequueño
+    bool result = grafo.xstAdy(vrtO, 2);
+    if (result == true ) {
+        std::cout << "%TEST_FAILED% FALLO EL METODO XstAdy1 (newsimpletest) message=error message sample" << std::endl;
+    }
+}
+
+//Retorna un apuntador nulo (cero o NULL) cuando la lista de adyacencias está vacía. 
+void testObtAdy1() {
     int vrt;
     Grafo grafo;
-    bool result = grafo.xstVrt(vrt);
+    int* result = grafo.obtAdy(vrt);
+    if (!(result ==0)) {
+        std::cout << "%TEST_FAILED% FALLO EL METODO ObtAdy1  (newsimpletest) message=error message sample" << std::endl;
+    }
+}
+
+// Retorna un apuntador a un arreglo de N posiciones cuando la lista de adyacencias tiene N elementos (basarse en Grafo(“grafo_pequenyo.txt”)
+void testObtAdy2() {
+    int vrt;
+    Grafo grafo;
+    int* result = grafo.obtAdy(vrt);
+    if (!(result ==) {
+        std::cout << "%TEST_FAILED% FALLO EL METODO ObtAdy2  (newsimpletest) message=error message sample" << std::endl;
+    }
+}
+
+//Retorna S cuando el vértice está en dicho estado.
+void testObtEst1() {
+    int vrt;
+    Grafo grafo;
+    E result = grafo.obtEst(vrt);
     if (true /*check result*/) {
-        std::cout << "%TEST_FAILED% time=0 testname=testXstVrt (newsimpletest) message=error message sample" << std::endl;
+        std::cout << "%TEST_FAILED% FALLO EL METODO ObtEst1 (newsimpletest) message=error message sample" << std::endl;
+    }
+}
+
+//Retorna I cuando el vértice está en dicho estado.
+void testObtEst2() {
+    int vrt;
+    Grafo grafo;
+    E result = grafo.obtEst(vrt);
+    if (true /*check result*/) {
+        std::cout << "%TEST_FAILED% FALLO EL METODO ObtEst2 (newsimpletest) message=error message sample" << std::endl;
+    }
+}
+
+//Retorna R cuando el vértice está en dicho estado.
+void testObtEst3() {
+    int vrt;
+    Grafo grafo;
+    E result = grafo.obtEst(vrt);
+    if (true /*check result*/) {
+        std::cout << "%TEST_FAILED% FALLO EL METODO ObtEst3 (newsimpletest) message=error message sample" << std::endl;
+    }
+}
+
+
+void testPromLongCmnsCrts1() {
+    Grafo grafo;
+    double result = grafo.promLongCmnsCrts();
+    if (true /*check result*/) {
+        std::cout << "%TEST_FAILED% FALLO EL METODO PromLongCmnsCrts1 (newsimpletest) message=error message sample" << std::endl;
+    }
+}
+
+void testPromLongCmnsCrts2() {
+    Grafo grafo;
+    double result = grafo.promLongCmnsCrts();
+    if (true /*check result*/) {
+        std::cout << "%TEST_FAILED% FALLO EL METODO PromLongCmnsCrts2 (newsimpletest) message=error message sample" << std::endl;
+    }
+}
+
+void testPromLongCmnsCrts3() {
+    Grafo grafo;
+    double result = grafo.promLongCmnsCrts();
+    if (true /*check result*/) {
+        std::cout << "%TEST_FAILED% FALLO EL METODO PromLongCmnsCrts3 (newsimpletest) message=error message sample" << std::endl;
+    }
+}
+
+void testCoeficienteAgrupamiento1() {
+    int vrt;
+    Grafo grafo;
+    double result = grafo.coeficienteAgrupamiento(vrt);
+    if (true /*check result*/) {
+        std::cout << "%TEST_FAILED% FALLO EL METODO CoeficienteAgrupamiento1 (newsimpletest) message=error message sample" << std::endl;
+    }
+}
+
+void testCoeficienteAgrupamiento2() {
+    int vrt;
+    Grafo grafo;
+    double result = grafo.coeficienteAgrupamiento(vrt);
+    if (true /*check result*/) {
+        std::cout << "%TEST_FAILED% FALLO EL METODO CoeficienteAgrupamiento2  (newsimpletest) message=error message sample" << std::endl;
     }
 }
 
@@ -158,7 +220,11 @@ int main(int argc, char** argv) {
     std::cout << "%SUITE_STARTED%" << std::endl;
 
     std::cout << "%TEST_STARTED% testGrafo (newsimpletest)" << std::endl;
-    testGrafo();
+    testConstructorGrafo1();
+    std::cout << "%TEST_FINISHED% time=0 testGrafo (newsimpletest)" << std::endl;
+
+    std::cout << "%TEST_STARTED% testGrafo (newsimpletest)" << std::endl;
+    testConstructorGrafo2();
     std::cout << "%TEST_FINISHED% time=0 testGrafo (newsimpletest)" << std::endl;
 
     std::cout << "%TEST_STARTED% testGrafo2 (newsimpletest)" << std::endl;
