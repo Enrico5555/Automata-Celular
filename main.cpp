19ccffc91882b5d3ab4873a497e6436130038b67
Konrad <konri9@gmail.com>
2015-05-31 15:33:06 -0600

    cambios del profe

--- main.cpp
+++ main.cpp
@@ -1,56 +1,101 @@
-/* 
+/*
  * File:   main.cpp
- * Author: Alan
+ * Author: alan.calderon
  *
- * Created on 1 de abril de 2015, 06:52 PM
+ * Created on 15 de abril de 2015, 12:05 PM
  */
-#include <cstdlib>
-#include <iostream>
-#include <string>
 
-#include "Parse.h"
-#include "Grafo.h"
-#include "Simulador.h"
-#include "Visualizador.h"
+// Ejemplo base tomado de cplusplus
+// normal_distribution example
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <chrono>
+#include <random>
 
 using namespace std;
-using namespace line_parse;
 
+void generarArchivoRed(int n, double prbAdy, string nomArch) {
+    // Se define y crea el generador de n�mero aleatorios basado en el reloj del sistema.
+    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+    std::default_random_engine generador(seed);
+    enum Bit { U, C, }; // U indica que s� hay adyacencia, C indica que no.
 
-/*
- * 
- */
+    Bit* matBits = new Bit[n*n]; // Guardaremos las adyacencias en una matriz representada por un arreglo.
 
-/*cargar “datosGrafo.txt” : que permitirá cargar los datos de un grafo desde un
-archivo de texto.
-2. crear #vrt #prbAdy : que creará un grafo con cntVrt == #vrt en el que cada
-adyacencia (i,j) tenga la probabilidad de existir de prbAdy.
-3. simular #cItr #ios #vsc #vcf #rc #grc : que simulará la infección (ver
-especificación) sin visualización de los estados intermedios, sólo la
-visualización de los estados inicial y final.
-4. simular-visualizar #cItr #ios #vsc #vcf #rc #grc : que simulará la infección (ver
-especificación) visualizando todos los estados intermedios, el inicial y el final.
-5. visualizar : que mostrará gráficamente el grafo previamente cargado o creado.
-Si no ha sido cargado ni creado el grafo, se mostrará la advertencia “DEBE
-CREAR O CARGAR GRAFO ANTES!!”6. calcular-promedio-longitud-caminos-cortos : que calculará el promedio de las
-longitudes de los caminos más cortos del grafo previamente cargado o
-asociado. Si no ha sido cargado ni creado el grafo, se mostrará la advertencia
-“DEBE CREAR O CARGAR GRAFO ANTES!!”
-7. calcular-centralidad-intermedial #vrt : que calculará la centralidad intermedial
-del vértice cuyo índice es #vrt (ver especificación) en el grafo asociado. Si no
-ha sido cargado ni creado el grafo, se mostrará la advertencia “DEBE CREAR
-O CARGAR GRAFO ANTES!!” NOTA: sigue pendiente para último laboratorio.
-8. calcular-coeficiente-agrupamiento #vrt : que calculará el coeficiente local de
-agrupamiento para vértice cuyo índice es #vrt en el grafo asociado. Si no ha
-sido cargado ni creado el grafo, se mostrará la advertencia “DEBE CREAR O
-CARGAR GRAFO ANTES!!”*/
+    // Se inicia la matriz en C.
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < n; j++) matBits[i*n + j] = C; // Fila i, columna j.
 
-int main(int argc, char** argv) {
-    Grafo mg(10,0.5); // Crea grafo al azar con 10 vértices y probabilidad de adyacencias de 0.5}
-    mg.infectar(3); // Pone en estado de infectados (I) a 3 vértices de mg.
-    mg.azarizarTmpChqVrs(5); // Asigna valores al azar entre 1 y 5 de temporizador de chequeo. 
-    Simulador s(mg); // Crea una instancia de Simulador.
-    Visualizador v(mg,s); // Crea una instancia de Visualizador
+    for (int i = 0; i < n; i++) {
+        // Esta distribuci�n servir� para generar la cantidad de adyacencias por v�rtice.
+        //std::normal_distribution<double> dst_normal(prmAdy, dsvEst);
+
+        for (int j = i; j < n; j++) { // OJO: desde j = i empieza
+            //std::default_random_engine generatorUniforme;
+            std::uniform_int_distribution<int> dst_uniforme(0, 100);
+            int xstAdyI = dst_uniforme(generador); // se genera un valor al azar entre 0 y 100.
+	    double xstAdyD = xstAdyI / 100.0; // se genera un valor al azar entre 0 y 1.
+            if ( xstAdyD <= prbAdy) { // determinamos si "nace" o no la adyacencia
+                    matBits[i*n + j] = U;
+                    matBits[j*n + i] = U; // agregamos la adyacencia "espejo"
+            }
+        }
+
+		int cntAdy = 0;	// Vamos a contar las adyacencias para asegurarnos que al menos hay una por cada v�rtice.
+		for (int j = 0; j < n; j++)
+			if ( matBits[i*n + j] == U )
+			    cntAdy++;
+
+		if ( cntAdy == 0 ) { // no queremos v�rtices aislados
+			std::uniform_int_distribution<int> dst_uniforme(0, n-1);
+			int indAdy = dst_uniforme(generador); // se genera un valor al azar entre 0 y n-1
+			matBits[i*n + indAdy] = U; // agregamos la adyacencia para tener al menos una
+            matBits[indAdy*n + i] = U; // agregamos la adyacencia "espejo"
+		};
+    }
+
+    // Ahora se crea el archivo con base en la matriz de bits
+    ofstream archSalida(nomArch.c_str(), ios::out); // se crea flujo de salida
+    if (!archSalida) { // operador ! sobrecargado
+        cerr << "No se pudo abrir el archivo de salida" << endl;
+        exit(1);
+    }
+    archSalida << n << " " << prbAdy << endl; // En la primera l�nea se anota la cantidad de v�rtices
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            if (matBits[i*n + j] == U) archSalida << j << ",";
+        }
+        archSalida << endl; // se cambia de l�nea
+    }
+    delete[] matBits;
+}
+
+int main() {
+    // Se construye el generador de n�meros al azar basado en una semilla tomada
+    // del reloj del sistema:
+    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+    std::default_random_engine generator(seed);
+
+    // Se construye la funci�n de distribuci�n normal.
+    // El primer par�metro es el promedio de la distribuci�n.
+    // El segundo par�metro es la distribuci�n est�ndar de la distribuci�n.
+    std::normal_distribution<double> distribution(0.0, 1.0);
+
+    std::cout << "Algunos n�meros al azar basados en la distribuci�n normal:" << std::endl;
+    for (int i = 0; i < 10; ++i)
+        std::cout << distribution(generator) << std::endl;
+
+    /* GENERACI�N DE ARCHIVOS DE DATOS */
+    // Genera un archivo de red con 10 v�rtices, con una probabilidad de adyacencia de 0.5.
+    generarArchivoRed(10, 0.5, "redMuyPeq.txt");
+
+    // Genera un archivo de red con 100 v�rtices, con una probabilidad de adyacencia de 0.25.
+    generarArchivoRed(100, 0.25, "redPeq.txt");
+
+    // Genera un archivo de red con 1000 v�rtices, con una probabilidad de adyacencia de 0.15.
+    generarArchivoRed(1000, 0.15, "redGrn.txt");
     return 0;
 }
 
